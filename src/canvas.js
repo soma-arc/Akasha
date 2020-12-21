d255aa9d1ac91b0b351a6e3f9880e6afdc96d305
soma_arc <somaarcr@gmail.com>
2020-12-21 23:10:47 +0900

    Save images

--- src/canvas.js
+++ src/canvas.js
@@ -88,6 +88,41 @@ export class Canvas2D {
         this.canvas.height = parent.clientHeight * this.pixelRatio;
         this.canvasRatio = this.canvas.width / this.canvas.height / 2;
     }
+
+        // https://stackoverflow.com/questions/37135417/download-canvas-as-png-in-fabric-js-giving-network-error
+    dataURLtoBlob (dataurl) {
+        const arr = dataurl.split(',');
+        const mime = arr[0].match(/:(.*?);/)[1];
+        const bstr = atob(arr[1]);
+        let n = bstr.length;
+        const u8arr = new Uint8Array(n);
+        while (n--) {
+            u8arr[n] = bstr.charCodeAt(n);
+        }
+        return new Blob([u8arr], { type: mime });
+    }
+
+    saveImage (gl, x, y, width, height, filename) {
+        const data = new Uint8Array(width * height * 4);
+        const type = gl.UNSIGNED_BYTE;
+        gl.readPixels(x, y, width, height, gl.RGBA, type, data);
+
+        const saveCanvas = document.createElement('canvas');
+        saveCanvas.width = width;
+        saveCanvas.height = height;
+        const context = saveCanvas.getContext('2d');
+
+        const imageData = context.createImageData(width, height);
+        imageData.data.set(data);
+        context.putImageData(imageData, 0, 0);
+        const a = document.createElement('a');
+        const canvasData = saveCanvas.toDataURL();
+        const blob = this.dataURLtoBlob(canvasData);
+
+        a.href = URL.createObjectURL(blob);
+        a.download = filename;
+        a.click();
+    }
 }
 
 export class RenderTextureCanvas extends Canvas2D {
